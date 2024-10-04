e56986d0c746b5475f90f21850d129cff62d472b
Justin Hoang <j124.dev@gmail.com>
2024-10-03 22:28:07 -0600

    fixing tmux custom config
    not sure about this line bc it errors out

--- home-manager/nixos/programs/tmux.nix
+++ home-manager/nixos/programs/tmux.nix
@@ -42,7 +42,6 @@
 
       resurrect_dir="$HOME/.tmux/resurrect"
       set -g @resurrect-dir $resurrect_dir
-      set -g @resurrect-hook-post-save-all "sed -i 's/--cmd lua.*--cmd set packpath/--cmd \"lua/g; s/--cmd set rtp.*\$/\"/' $resurrect_dir/last"
       set -g @resurrect-capture-pane-contents 'on'
       set -g @resurrect-processes '"~nvim"'
 
