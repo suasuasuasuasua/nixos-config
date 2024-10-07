64a3e37962f55a6e30b79f5925dba6da0696131b
Justin Hoang <j124.dev@proton.me>
2024-10-07 08:57:40 -0600

    ran the nix flake check and removed dead code

--- home-manager/desktop/gnome.nix
+++ home-manager/desktop/gnome.nix
@@ -1,47 +1,49 @@
 {
   pkgs,
-  lib,
+  # lib,
   ...
 }: {
   dconf = {
     enable = true;
-    settings = let
-      inherit (lib.hm.gvariant) mkValue mkVariant mkArray mkEmptyArray mkTuple mkString mkBoolean;
-    in {
-      # Change the desktop interface
-      "org/gnome/desktop/interface" = {
-        show-battery-percentage = true;
-        color-scheme = "prefer-dark";
+    settings =
+      # let
+      #   inherit (lib.hm.gvariant) mkValue mkVariant mkArray mkEmptyArray mkTuple mkString mkBoolean;
+      # in
+      {
+        # Change the desktop interface
+        "org/gnome/desktop/interface" = {
+          show-battery-percentage = true;
+          color-scheme = "prefer-dark";
 
-        monospace-font-name = "JetBrainsMono Nerd Font 10";
+          monospace-font-name = "JetBrainsMono Nerd Font 10";
+        };
+
+        # TODO: figure out the stupid gVariant bs
+        # "org/gnome/mutter/dynamic-workspaces" = (mkString true);
+
+        # favorite-apps=['org.gnome.Nautilus.desktop', 'firefox.desktop', 'Alacritty.desktop', 'thunderbird.desktop', 'obsidian.desktop', 'eleme       nt-desktop.desktop', 'discord.desktop']
+
+        # # Define the apps on the dock
+        # "org/gnome/shell/favorite-apps" =
+        # mkArray [
+        # "org.gnome.Nautilus.desktop"
+        # "firefox.desktop"
+        # "spotify.desktop"
+        # "Alacritty.desktop"
+        # "thunderbird.desktop"
+        # "obsidian.desktop"
+        # "element-desktop.desktop"
+        # ];
+
+        # Enable extensions
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = with pkgs.gnomeExtensions; [
+            tray-icons-reloaded.extensionUuid
+            open-bar.extensionUuid
+          ];
+        };
       };
-
-      # TODO: figure out the stupid gVariant bs
-      # "org/gnome/mutter/dynamic-workspaces" = (mkString true);
-
-      # favorite-apps=['org.gnome.Nautilus.desktop', 'firefox.desktop', 'Alacritty.desktop', 'thunderbird.desktop', 'obsidian.desktop', 'eleme       nt-desktop.desktop', 'discord.desktop']
-
-      # # Define the apps on the dock
-      # "org/gnome/shell/favorite-apps" =
-      # mkArray [
-      # "org.gnome.Nautilus.desktop"
-      # "firefox.desktop"
-      # "spotify.desktop"
-      # "Alacritty.desktop"
-      # "thunderbird.desktop"
-      # "obsidian.desktop"
-      # "element-desktop.desktop"
-      # ];
-
-      # Enable extensions
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = with pkgs.gnomeExtensions; [
-          tray-icons-reloaded.extensionUuid
-          open-bar.extensionUuid
-        ];
-      };
-    };
   };
 
   gtk = {
