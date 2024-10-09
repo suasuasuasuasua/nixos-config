001915f33ed9c7753cf2f02d5ecc4837b0e00711
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2024-10-09 08:39:30 -0600

    fix(devenv.nix): added a devenv setup
    using devenv to manage the pre-commits and env shell

---
+++ devenv.nix
@@ -0,0 +1,45 @@
+{pkgs, ...}: {
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    fastfetch
+
+    pre-commit
+    commitizen
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks = {
+    # Nix
+    alejandra.enable = true;
+    deadnix.enable = true;
+    flake-checker.enable = true;
+
+    # Git
+    commitizen.enable = true;
+    ripsecrets.enable = true;
+
+    # Docs
+    typos.enable = true;
+    markdownlint.enable = true;
+
+    # General
+    check-added-large-files.enable = true;
+    check-merge-conflicts.enable = true;
+    end-of-file-fixer.enable = true;
+    trim-trailing-whitespace.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
