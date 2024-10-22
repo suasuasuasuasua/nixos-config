b94828ab3aa907a351da3ea780b649bf5c61e591
Justin Hoang <j124.dev@proton.me>
2024-10-21 23:08:25 -0600

    feat(modules/desktop/kde.nix): added starter kde setup
    will revise the excluded and included modules

---
+++ modules/desktop/kde.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.defaultSession = "plasma";
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    konsole
+    oxygen
+  ];
+}
