9eaa033415397ae983bb17e2599fdcdc750ff70e
Justin Hoang <j124.dev@proton.me>
2024-11-06 21:43:33 -0700

    feat(modules/storage.nix): added auto-optimise and garbage collection
    should hopefully help with storage!

--- modules/storage.nix
+++ modules/storage.nix
@@ -1,4 +1,6 @@
 {
+  # https://nixos.wiki/wiki/Storage_optimization
+
   # Optimize the storage once in a while
   nix.optimise.automatic = true;
   nix.optimise.dates = [
@@ -6,4 +8,14 @@
     "00:00"
     "12:00"
   ];
+
+  # Optimise on each build -- results in slower build :)
+  nix.settings.auto-optimise-store = true;
+
+  # Automatic garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
