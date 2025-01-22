732ca0a5dc33521c13fdb4efac1a836a6a0db17d
Justin Hoang <j124.dev@proton.me>
2025-01-21 17:28:25 -0700

    feat: using ly for displaymanager
    i like the simplicity of ly

--- modules/nixos/desktop/kde.nix
+++ modules/nixos/desktop/kde.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.ly.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.displayManager.defaultSession = "plasma";
 
