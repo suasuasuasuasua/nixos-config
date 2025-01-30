0b7faec3efce04824ea494c0d5ecddef9f5a064e
Justin Hoang <j124.dev@proton.me>
2025-01-30 16:28:18 -0700

    fix: added firewall and ports for server

--- configurations/nixos/laboratory/system/networking.nix
+++ configurations/nixos/laboratory/system/networking.nix
@@ -5,6 +5,26 @@
 
   networking.networkmanager.enable = true;
 
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      # SSH
+      22
+
+      # HTTP/S
+      80
+      443
+
+      # Samba
+      139
+      445
+    ];
+    allowedUDPPorts = [
+      # Samba
+      137
+      138
+    ];
+  };
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
