c882667b67dc416ebebb755f9b1a39b60df1149f
Justin Hoang <j124.dev@proton.me>
2025-01-31 08:23:04 -0700

    fix: added pinging for firewall

--- configurations/nixos/laboratory/system/networking.nix
+++ configurations/nixos/laboratory/system/networking.nix
@@ -8,6 +8,7 @@
   # TODO: figure out the firewall more
   networking.firewall = {
     enable = true;
+    allowPing = true;
   };
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
