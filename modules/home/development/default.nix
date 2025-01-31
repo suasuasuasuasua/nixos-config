36e6ce7d28dc2696e6c62b3a849542f587da8e7c
Justin Hoang <j124.dev@proton.me>
2025-01-30 21:11:17 -0700

    fix: only import neovim by default!
    sometimes messes up the macos install...

--- modules/home/development/default.nix
+++ modules/home/development/default.nix
@@ -1,8 +1,6 @@
-# A module that automatically imports everything else in the parent folder.
+# By default, only do neovim!
 {
-  imports =
-    with builtins;
-    map
-      (fn: ./${fn})
-      (filter (fn: fn != "default.nix") (attrNames (readDir ./.)));
+  imports = [
+    ./neovim
+  ];
 }
