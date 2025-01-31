21c1de470bc56ad17a6b0dfe43cd6dafe3293272
Justin Hoang <j124.dev@proton.me>
2025-01-31 08:23:41 -0700

    fix: testing out samba share again

--- modules/nixos/services/samba.nix
+++ modules/nixos/services/samba.nix
@@ -10,14 +10,24 @@
       "server string" = "smbnix";
       "netbios name" = "smbnix";
       "security" = "user";
+      #"use sendfile" = "yes";
+      #"max protocol" = "smb2";
+      # note: localhost is the ipv6 localhost ::1
+      "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+      "hosts deny" = "0.0.0.0/0";
+      "guest account" = "nobody";
+      "map to guest" = "bad user";
     };
-    # Videos, movies, etc.
-    media = {
-      "path" = "/zshare/media/";
+    # Music videos and audio!
+    music = {
+      "path" = "/zshare/media/music";
       "browseable" = "yes";
       "read only" = "no";
-      "writeable" = "yes";
       "guest ok" = "yes";
+      "create mask" = "0644";
+      "directory mask" = "0755";
+      # remember to create the samba group and add recursive permissions!
+      "force group" = "samba";
     };
   };
 
