43cdc496e2e890f87dcd4b2fd40326ff539cee05
Justin Hoang <j124.dev@proton.me>
2025-01-30 20:04:24 -0700

    feat: added mealie recipe manager

--- configurations/nixos/laboratory/services.nix
+++ configurations/nixos/laboratory/services.nix
@@ -8,13 +8,15 @@ in
   imports = [
     (self + /modules/nixos/services/nginx.nix)
     (self + /modules/nixos/services/dashy.nix)
+    # Port 9090
+    (self + /modules/nixos/services/cockpit.nix)
 
     # Port 3000
     (self + /modules/nixos/services/adguardhome.nix)
     # Port 8096
     (self + /modules/nixos/services/jellyfin.nix)
-    # Port 9090
-    (self + /modules/nixos/services/cockpit.nix)
+    # Port 9000
+    (self + /modules/nixos/services/mealie.nix)
 
     (self + /modules/nixos/services/samba.nix)
   ];
---
+++ modules/nixos/services/mealie.nix
@@ -0,0 +1,8 @@
+{
+  services.mealie = {
+    enable = true;
+    # default port = 9000
+    port = 9000;
+    settings = { };
+  };
+}
