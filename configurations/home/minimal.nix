9a902a26a9319b61f2097c832ac9eb97d5ff563e
Justin Hoang <j124.dev@proton.me>
2025-01-30 21:10:39 -0700

    feat: added minimal profile for quick debugging and setup

---
+++ configurations/home/minimal.nix
@@ -0,0 +1,18 @@
+{ flake, pkgs, lib, ... }:
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in
+{
+  imports = [
+    # no imports!
+    (self + "/modules/home/shell.nix")
+  ];
+
+  # Allow unfree packages like VSCode
+  nixpkgs.config.allowUnfree = true;
+
+  home.username = "justinhoang";
+  home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/justinhoang";
+  home.stateVersion = "24.11";
+}
