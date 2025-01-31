6189412572c57e5de89e247707ce55f3280972e7
Justin Hoang <j124.dev@proton.me>
2025-01-30 19:47:52 -0700

    fix: allow for password ssh authentication on server

--- configurations/nixos/laboratory/system/networking.nix
+++ configurations/nixos/laboratory/system/networking.nix
@@ -5,6 +5,10 @@
 
   networking.networkmanager.enable = true;
 
+  # TODO: figure out the firewall more
+  networking.firewall = {
+    enable = true;
+  };
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
@@ -14,7 +18,7 @@
       PermitRootLogin = "no";
       # Opinionated: use keys only.
       # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
+      PasswordAuthentication = true;
     };
   };
 }
