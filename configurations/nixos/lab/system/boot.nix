9c072fab3052b4d96f7d8e856eac194aff645ee7
Justin Hoang <j124.dev@proton.me>
2025-02-20 21:04:39 -0700

    build: updated lab linux kernel to 6.12

--- configurations/nixos/lab/system/boot.nix
+++ configurations/nixos/lab/system/boot.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   # Use the systemd-boot EFI boot loader.
   #
@@ -10,6 +11,10 @@
   };
 
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # 6.12 faster than 6.6 (https://www.phoronix.com/review/linux-66-612-lts)
+  # current LTS on 24.11 if 6.6, so we can go back later in May 25
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
 
   # Alternative that I'll keep around. Basically, the problem was the
   # boot.loader.grub.device was being set to an incorrect value when I was
--- configurations/nixos/lab/system/zfs.nix
+++ configurations/nixos/lab/system/zfs.nix
@@ -1,9 +1,5 @@
-{ pkgs, ... }:
 {
   services.zfs.autoScrub.enable = true;
   services.zfs.autoSnapshot.enable = true;
   services.zfs.trim.enable = true;
-
-  # Ensure that the kernel is compatible with zfs
-  boot.kernelPackages = pkgs.linuxPackages_6_6; # update to LTS as we go
 }
