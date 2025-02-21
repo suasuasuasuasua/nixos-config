237358e492f35f826a3a1ff9966fb49844a18bc9
Justin Hoang <j124.dev@proton.me>
2025-02-20 21:29:58 -0700

    build: update raspberry pi linux kernel to 6.12

--- configurations/nixos/pi/system/pi.nix
+++ configurations/nixos/pi/system/pi.nix
@@ -4,6 +4,10 @@
   # https://github.com/nix-community/raspberry-pi-nix/blob/master/rpi/default.nix
   raspberry-pi-nix = {
     board = "bcm2711";
+    # refer to flake.lock for rpi-linux version
+    # - workaround until https://github.com/nix-community/raspberry-pi-nix/issues/116
+    # kernel-version = "v6_6_67"; # 6.6 LTS
+    kernel-version = "v6_12_11"; # 6.12 LTS
     # TODO: why isn't this cached? faster build if i disable
     # i don't have cameras anyway
     libcamera-overlay = {
