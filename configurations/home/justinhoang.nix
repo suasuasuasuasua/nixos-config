9ef616ea33b9f483b7730d9e4de49a8642736719
Justin Hoang <j124.dev@proton.me>
2025-02-27 22:45:26 -0700

    chore: removed extraneous nixpkgs config
    home manager already uses global config (thanks nixos-unified)

--- configurations/home/justinhoang.nix
+++ configurations/home/justinhoang.nix
@@ -4,9 +4,6 @@
   ...
 }:
 {
-  # Allow unfree packages like VSCode
-  nixpkgs.config.allowUnfree = true;
-
   home = {
     username = "justinhoang";
     homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/justinhoang";
