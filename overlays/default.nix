78cde0d0adf59131fd6f22e46faf3abed5f352c5
Justin Hoang <j124.dev@proton.me>
2025-03-01 00:39:02 -0700

    fix: added darwin overlay for nixpkgs-darwin
    forgot to add this earlier

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,7 +4,14 @@ let
 in
 # deadnix: skip
 final: prev: {
+  # pkgs.unstable.X
   unstable = import inputs.nixpkgs-unstable {
+    inherit prev;
+    inherit (prev) system;
+    config.allowUnfree = true;
+  };
+  # pkgs.nix-darwin.X
+  darwin = import inputs.nixpkgs-darwin {
     inherit prev;
     inherit (prev) system;
     config.allowUnfree = true;
