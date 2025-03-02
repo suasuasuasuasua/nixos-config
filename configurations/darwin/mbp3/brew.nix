7185046344e3a759b8a66914ed4ab28e5af0b7d1
Justin Hoang <j124.dev@proton.me>
2025-03-02 14:38:41 -0700

    docs: added comments for brew apps

--- configurations/darwin/mbp3/brew.nix
+++ configurations/darwin/mbp3/brew.nix
@@ -2,6 +2,7 @@
   homebrew = {
     enable = true;
     onActivation = {
+      # don't upgrade automatically -- let's do manual brew upgrades
       upgrade = false;
       autoUpdate = false;
       # uninstall brew apps not specified here
@@ -41,25 +42,25 @@
     ];
     masApps = {
       # dev
-      xcode = 497799835;
+      xcode = 497799835; # macOS, iOS, etc. IDE
 
       # entertainment
-      audible-audio-entertainment = 379693831;
-      amazon-kindle = 302584613;
+      audible-audio-entertainment = 379693831; # audible client
+      amazon-kindle = 302584613; # kindle client
 
       # productivity
-      flow-focus-pomodoro-timer = 1423210932;
-      imovie = 408981434;
-      keynote = 409183694;
-      notability-smarter-ai-notes = 360593530;
-      numbers = 409203825;
-      pages = 409201541;
-      tot = 1491071483;
+      flow-focus-pomodoro-timer = 1423210932; # pomodoro timer
+      imovie = 408981434; # apple movie editor and maker
+      keynote = 409183694; # apple presentation suite
+      notability-smarter-ai-notes = 360593530; # note taking app
+      numbers = 409203825; # apple spreadsheet application
+      pages = 409201541; # apple word processing application
+      tot = 1491071483; # daily note and task tracker
 
       # utility
-      dark-reader-for-safari = 1438243180;
-      hidden-bar = 1452453066;
-      magnet = 441258766;
+      dark-reader-for-safari = 1438243180; # dark mode for safari
+      hidden-bar = 1452453066; # macOS menubar organizer
+      magnet = 441258766; # macOS workspace organizer
       # vimari = ""  # TODO: gone from mac app store?
     };
   };
