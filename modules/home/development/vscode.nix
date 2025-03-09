08fadca7dd762bb62758850073754878dc556d6d
Justin Hoang <j124.dev@proton.me>
2025-03-09 00:06:19 -0700

    chore: attempting to add settings for vscode

--- modules/home/development/vscode.nix
+++ modules/home/development/vscode.nix
@@ -16,22 +16,66 @@ in
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
+      # # get and update packages through nixpkgs
+      # mutableExtensionsDir = false;
       extensions = with pkgs.vscode-extensions; [
         # General
-        vscodevim.vim
-        gruntfuggly.todo-tree
         codezombiech.gitignore
+        gruntfuggly.todo-tree
+        vscodevim.vim
+        waderyan.gitblame
 
-        # Remote
-        ms-vscode-remote.remote-ssh
-        ms-vscode-remote.remote-ssh-edit
-        ms-vscode-remote.remote-wsl
-        ms-vscode-remote.remote-containers
+        # Langs
+        ms-python.python
 
         # Nix Dev
         jnoortheen.nix-ide
         mkhl.direnv
+
+        # Remote
+        ms-vscode-remote.remote-containers
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit
+        ms-vscode-remote.remote-wsl
       ];
+      userSettings = {
+        # vim setup
+        vim = {
+          "cursorStylePerMode.normal" = "block";
+          "cursorStylePerMode.replace" = "underline";
+          "cursorStylePerMode.visual" = "underline";
+          "cursorStylePerMode.visualblock" = "underline";
+          "cursorStylePerMode.visualline" = "underline";
+          "foldfix" = true;
+          "highlightedyank.textColor" = "'blue'";
+          "insertModeKeyBindings" = [
+            {
+              "after" = [
+                "<Esc>"
+              ];
+              "before" = [
+                "j"
+                "k"
+              ];
+            }
+          ];
+          "joinspaces" = false;
+          "leader" = "\\ ";
+          "normalModeKeyBindings" = [
+            {
+              "before" = [
+                "c"
+                "d"
+              ];
+              "commands" = [
+                {
+                  "command" = "editor.action.rename";
+                }
+              ];
+            }
+          ];
+        };
+      };
     };
   };
 }
