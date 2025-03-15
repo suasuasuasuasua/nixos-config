4243939caf7580e33e4a4aa6e7504f7a7ac58a54
Justin Hoang <j124.dev@proton.me>
2025-03-14 23:48:35 -0600

    fix: debugged graphics issues with nvidia gpu on wake
    good thing i only have 20+ series cards lol...
    i should generalize this module more though

--- modules/nixos/gpu/nvidia.nix
+++ modules/nixos/gpu/nvidia.nix
@@ -36,15 +36,18 @@ in
         # Modesetting is required.
         modesetting.enable = true;
 
-        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-        # Enable this if you have graphical corruption issues or application crashes after waking
-        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-        # of just the bare essentials.
-        powerManagement.enable = false;
+        powerManagement = {
+          # Nvidia power management. Experimental, and can cause sleep/suspend
+          # to fail. Enable this if you have graphical corruption issues or
+          # application crashes after waking up from sleep. This fixes it by
+          # saving the entire VRAM memory to /tmp/ instead of just the bare
+          # essentials.
+          enable = true; # TODO: graphical issues on legion...
 
-        # Fine-grained power management. Turns off GPU when not in use.
-        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-        powerManagement.finegrained = false;
+          # Fine-grained power management. Turns off GPU when not in use.
+          # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+          finegrained = false; # TODO: true bc rtx 2080 is turing? but needs offload
+        };
 
         # Use the NVidia open source kernel module (not to be confused with the
         # independent third-party "nouveau" open source driver).
@@ -53,7 +56,7 @@ in
         # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
         # Only available from driver 515.43.04+
         # Currently alpha-quality/buggy, so false is currently the recommended setting.
-        open = false;
+        open = true; # TODO: rtx 2080 is compatible
 
         # Enable the Nvidia settings menu,
         # accessible via `nvidia-settings`.
@@ -66,6 +69,7 @@ in
         # 2. Under 'bus info', translate the bus ID hexadecimal to decimal and format:
         #    pci@0000:0e:00.0 -> PCI:14:0:0
         #    - Do this step for both the Nvidia/AMD and integrated GPU
+        # TODO: not just for laptops?
         prime = lib.mkIf cfg.laptop.enable {
           offload = {
             enable = true;
