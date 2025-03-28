1f011845fa5a372891c232ff7558a6644398ffe9
Justin Hoang <j124.dev@proton.me>
2025-03-28 08:18:20 -0600

    refactor: moved nix config to top level

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,4 +1,13 @@
-{ config, pkgs, ... }:
+{
+  flake,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  inherit (flake) self;
+in
 {
   # write a list of system packages to /etc/current-system-packages
   environment.etc."current-system-packages".text =
@@ -8,4 +17,36 @@
       formatted = builtins.concatStringsSep "\n" sortedUnique;
     in
     formatted;
+
+  nixpkgs = {
+    overlays = lib.attrValues self.overlays;
+
+    # allows us to install apps like vscode
+    config.allowUnfree = true;
+  };
+
+  nix = {
+    # Choose from https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=nix
+    # package = pkgs.nixVersions.latest;
+
+    nixPath = [ "nixpkgs=${flake.inputs.nixpkgs}" ]; # Enables use of `nix-shell -p ...` etc
+    # TODO: figure out what this option even is
+    # registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
+    # Opinionated: disable channels
+
+    channel.enable = false;
+
+    settings = {
+      max-jobs = "auto";
+      experimental-features = "nix-command flakes";
+      # I don't have an Intel mac.
+      extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
+      # Nullify the registry for purity.
+      flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
+      trusted-users = [
+        "root"
+        "justinhoang" # TODO: make this dynamic
+      ];
+    };
+  };
 }
