49b035905afc5473b8c9b7182a84862768cf9188
Justin Hoang <j124.dev@proton.me>
2025-03-28 13:33:41 -0600

    docs: added comments for flake-parts modules

--- modules/flake-parts/githooks.nix
+++ modules/flake-parts/githooks.nix
@@ -6,24 +6,24 @@
   perSystem = _: {
     pre-commit.settings.hooks = {
       # Nix
-      nixfmt-rfc-style.enable = true;
-      deadnix.enable = true; # kind of annoying in practice
-      flake-checker.enable = true;
-      statix.enable = true;
-      nil.enable = true;
+      nixfmt-rfc-style.enable = true; # format nix files to rfc standards
+      deadnix.enable = true; # remove any unused variabes and imports
+      flake-checker.enable = true; # run `flake check`
+      statix.enable = true; # check "good practices" for nix
+      nil.enable = true; # lsp that also has formatter
 
       # Git
-      commitizen.enable = true;
-      ripsecrets.enable = true;
+      commitizen.enable = true; # ensure conventional commits standard
+      ripsecrets.enable = true; # remove any hardcoded secrets
 
       # Docs
-      markdownlint.enable = true;
+      markdownlint.enable = true; # format markdown files
 
       # General
-      check-added-large-files.enable = true;
-      check-merge-conflicts.enable = true;
-      end-of-file-fixer.enable = true;
-      trim-trailing-whitespace.enable = true;
+      check-added-large-files.enable = true; # warning about large files (lfs?)
+      check-merge-conflicts.enable = true; # don't commit merge conflicts
+      end-of-file-fixer.enable = true; # add a line at the end of the file
+      trim-trailing-whitespace.enable = true; # trim trailing whitespace
     };
   };
 }
