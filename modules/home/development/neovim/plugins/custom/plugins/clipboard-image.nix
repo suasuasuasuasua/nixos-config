f6e0bd21cbe1663a10b6a352f08cc7a7a30e8428
Justin Hoang <j124.dev@proton.me>
2025-03-31 11:12:54 -0600

    feat: added clipboard-image for neovim
    useful for pasting in markdown

---
+++ modules/home/development/neovim/plugins/custom/plugins/clipboard-image.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  name = "clipboard-image";
+  cfg = config.home.development.neovim.plugins.${name};
+in
+{
+  options.home.development.neovim.plugins.${name} = {
+    enable = lib.mkEnableOption "Enable ${name} plugin for neovim";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.nixvim = {
+      plugins.clipboard-image = {
+        enable = true;
+      };
+    };
+  };
+}
