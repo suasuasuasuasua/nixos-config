2ebce594ef83f2f634c6ae87feaa85f3060ee465
Justin Hoang <j124.dev@proton.me>
2025-04-03 18:25:57 -0600

    feat: added basic firefox config with extensions and preferences
    - need to flesh out productivity profile
    - extensions yay (but fucking scuffed)
    - need to explore more settings

--- modules/home/gui/firefox.nix
+++ modules/home/gui/firefox.nix
@@ -6,6 +6,50 @@
 }:
 let
   cfg = config.home.gui.firefox;
+
+  settings = {
+    # Enable HTTPS-Only Mode
+    "dom.security.https_only_mode" = true;
+    "dom.security.https_only_mode_ever_enabled" = true;
+
+    # Disable updates (pretty pointless with nix)
+    "app.update.channel" = "default";
+
+    # Privacy settings
+    "privacy.donottrackheader.enabled" = true;
+    "privacy.trackingprotection.enabled" = true;
+    "privacy.trackingprotection.socialtracking.enabled" = true;
+    "privacy.partition.network_state.ocsp_cache" = true;
+
+    # Disable all sorts of telemetry
+    "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+    "browser.newtabpage.activity-stream.telemetry" = false;
+    "browser.ping-centre.telemetry" = false;
+    "toolkit.telemetry.archive.enabled" = false;
+    "toolkit.telemetry.bhrPing.enabled" = false;
+    "toolkit.telemetry.enabled" = false;
+    "toolkit.telemetry.firstShutdownPing.enabled" = false;
+    "toolkit.telemetry.hybridContent.enabled" = false;
+    "toolkit.telemetry.newProfilePing.enabled" = false;
+    "toolkit.telemetry.reportingpolicy.firstRun" = false;
+    "toolkit.telemetry.shutdownPingSender.enabled" = false;
+    "toolkit.telemetry.unified" = false;
+    "toolkit.telemetry.updatePing.enabled" = false;
+
+    # As well as Firefox 'experiments'
+    "experiments.activeExperiment" = false;
+    "experiments.enabled" = false;
+    "experiments.supported" = false;
+    "network.allow-experiments" = false;
+
+    # Disable Pocket Integration
+    "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+    "extensions.pocket.enabled" = false;
+    "extensions.pocket.api" = "";
+    "extensions.pocket.oAuthConsumerKey" = "";
+    "extensions.pocket.showHome" = false;
+    "extensions.pocket.site" = "";
+  };
 in
 {
   options.home.gui.firefox = {
@@ -18,6 +62,52 @@ in
       # TODO: firefox is in unstable for darwin...remove in may 2025
       package = with pkgs; if stdenv.isDarwin then unstable.firefox else firefox;
       # TODO: figure out profiles
+      profiles.personal = {
+        inherit settings;
+
+        id = 0;
+        bookmarks = [
+          {
+            name = "wikipedia";
+            tags = [ "wiki" ];
+            keyword = "wiki";
+            url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+          }
+          {
+            name = "kernel.org";
+            url = "https://www.kernel.org";
+          }
+          {
+            name = "Nix sites";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "homepage";
+                url = "https://nixos.org/";
+              }
+              {
+                name = "wiki";
+                tags = [
+                  "wiki"
+                  "nix"
+                ];
+                url = "https://wiki.nixos.org/";
+              }
+            ];
+          }
+        ];
+        # TODO: add firefox extensions
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          betterttv
+          ublock-origin
+          vimium
+        ];
+      };
+      profiles.productivity = {
+        inherit settings;
+
+        id = 1;
+      };
     };
   };
 }
