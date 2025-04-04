b3bf648a294c0a42855c8b23775085cb15c39867
Justin Hoang <j124.dev@proton.me>
2025-04-04 11:47:18 -0600

    docs(mbp3): added notes on home.nix

--- configurations/darwin/mbp3/home.nix
+++ configurations/darwin/mbp3/home.nix
@@ -24,76 +24,79 @@ in
     # TODO: if this gets too complex/long, modularize into folders
     config.home = {
       cli = {
-        bat.enable = true;
-        comma.enable = true;
-        devenv.enable = true;
-        direnv.enable = true;
-        fzf.enable = true;
-        git.enable = true;
-        github.enable = true;
-        gnupg.enable = true;
-        tmux.enable = true;
+        bat.enable = true; # better cat
+        comma.enable = true; # try out programs with `,`
+        devenv.enable = true; # easy dev environemnts
+        direnv.enable = true; # switch dev environments with .envrc files
+        fzf.enable = true; # fuzzy finder
+        git.enable = true; # source control
+        github.enable = true; # github cli integration
+        gnupg.enable = true; # gpg key signing
+        tmux.enable = true; # terminal multiplexer
       };
 
       development = {
         neovim = {
-          enable = true;
+          enable = true; # enable nixvim config
           # enable LSPs for _server-like_ things
           lsp = {
-            bashls.enable = true;
-            clangd.enable = true;
-            cssls.enable = true;
-            docker_compose_language_service.enable = true;
-            dockerls.enable = true;
-            gopls.enable = true;
-            html.enable = true;
-            jsonls.enable = true;
-            nginx_language_server.enable = true;
-            ltex.enable = true;
-            marksman.enable = true;
-            nil_ls.enable = true;
-            # nixd.enable = true;
-            pyright.enable = true;
-            ruff.enable = true;
-            rust_analyzer.enable = true;
-            sqls.enable = true;
-            tailwindcss.enable = true;
-            taplo.enable = true;
-            texlab.enable = true;
-            tinymist.enable = true;
-            yamlls.enable = true;
+            bashls.enable = true; # bash
+            clangd.enable = true; # c/c++
+            cssls.enable = true; # css
+            docker_compose_language_service.enable = true; # docker compose
+            dockerls.enable = true; # dockerfile
+            gopls.enable = true; # go
+            html.enable = true; # html
+            jsonls.enable = true; # json
+            nginx_language_server.enable = true; # nginx
+            ltex.enable = true; # latex
+            marksman.enable = true; # markdown
+            nil_ls.enable = true; # nix
+            # nixd.enable = true; # nix
+            pyright.enable = true; # python
+            ruff.enable = true; # python
+            rust_analyzer.enable = true; # rust
+            sqls.enable = true; # sql
+            tailwindcss.enable = true; # tailwindcss
+            taplo.enable = true; # toml
+            texlab.enable = true; # latex
+            tinymist.enable = true; # typst
+            yamlls.enable = true; # yaml
           };
           plugins = {
-            airline.enable = true;
-            arrow.enable = true;
-            auto-save.enable = true;
-            auto-session.enable = true;
-            clipboard-image.enable = true;
-            diffview.enable = true;
-            lazygit.enable = true;
-            markdown-preview.enable = true;
-            neogit.enable = true;
+            airline.enable = true; # status line
+            arrow.enable = true; # bookmarks (;)
+            auto-save.enable = true; # autosave the file
+            # auto-session.enable = true; # auto gen/lod a session file
+            barbar.enable = true; # better tabs
+            clipboard-image.enable = true; # paste images from clipboard
+            diffview.enable = true; # show git-rev diffs
+            dropbar.enable = true; # breadcrumb navigation
+            lazygit.enable = true; # lazygit integration
+            markdown-preview.enable = true; # local live markdown preview server
+            neogit.enable = true; # git integration (secondary to lazygit)
             ollama = {
+              # ollama llm integration
               enable = true;
               model = "gemma3";
               url = "http://127.0.0.1:11434"; # local ollama instance
             };
-            render-markdown.enable = true;
-            repeat.enable = true;
-            surround.enable = true;
-            tmux.enable = true;
-            treesitter.enable = true;
-            typst.enable = true;
-            ufo.enable = true;
-            vimtex.enable = true;
-            zen-mode.enable = true;
+            render-markdown.enable = true; # render markdown in the terminal
+            repeat.enable = true; # repeat edits (.)
+            surround.enable = true; # surround operations
+            tmux.enable = true; # tmux navigation integration
+            treesitter.enable = true; # treesitter extensions integration
+            typst.enable = true; # typst preview and syntax
+            ufo.enable = true; # better folding support
+            vimtex.enable = true; # latex support
+            zen-mode.enable = true; # zen mode support
           };
         };
-        zsh.enable = true;
+        zsh.enable = true; # preferred shell
       };
 
       gui = {
-        firefox.enable = true;
+        firefox.enable = true; # browser
         spotify.enable = true; # music platform
         vscode.enable = true; # text editor
       };
