9db80192878679625009a2875335f24a8f8793c1
Justin Hoang <j124.dev@proton.me>
2025-04-04 15:10:45 -0600

    chore(nixvim): rearrange startup screen

--- modules/home/development/neovim/plugins/mini.nix
+++ modules/home/development/neovim/plugins/mini.nix
@@ -49,10 +49,10 @@
               __raw = "require('mini.starter').sections.builtin_actions()";
             };
             "__unkeyed-2.recent_files_current_directory" = {
-              __raw = "require('mini.starter').sections.recent_files(10, false)";
+              __raw = "require('mini.starter').sections.recent_files(10, true)";
             };
             "__unkeyed-3.recent_files" = {
-              __raw = "require('mini.starter').sections.recent_files(10, true)";
+              __raw = "require('mini.starter').sections.recent_files(10, false)";
             };
             "__unkeyed-4.sessions" = {
               __raw = "require('mini.starter').sections.sessions(5, true)";
