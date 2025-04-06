bba179a1f27a74b9c818a963825b549140f2bf04
Justin Hoang <j124.dev@proton.me>
2025-04-06 15:06:41 -0600

    feat(nixvim): added plugin that separates buffers by tab
    separation of concern is better

---
+++ modules/home/development/neovim/plugins/custom/plugins/scope.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  name = "scope";
+  cfg = config.home.development.neovim.plugins.${name};
+in
+{
+  options.home.development.neovim.plugins.${name} = {
+    enable = lib.mkEnableOption "Enable ${name} plugin for neovim";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.nixvim = {
+      plugins.scope = {
+        enable = true;
+      };
+
+      opts = {
+        termguicolors = true;
+      };
+    };
+  };
+}
