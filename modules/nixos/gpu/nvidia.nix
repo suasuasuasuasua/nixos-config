9d3244c2378cceb2cf02023e1ea221858cc5df61
Justin Hoang <j124.dev@proton.me>
2025-04-12 17:32:35 -0600

    style(nvidia): remove old TODOs

--- modules/nixos/gpu/nvidia.nix
+++ modules/nixos/gpu/nvidia.nix
@@ -42,11 +42,11 @@ in
           # application crashes after waking up from sleep. This fixes it by
           # saving the entire VRAM memory to /tmp/ instead of just the bare
           # essentials.
-          enable = true; # TODO: graphical issues on legion...
+          enable = true;
 
           # Fine-grained power management. Turns off GPU when not in use.
           # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-          finegrained = false; # TODO: true bc rtx 2080 is turing? but needs offload
+          finegrained = false;
         };
 
         # Use the NVidia open source kernel module (not to be confused with the
@@ -56,7 +56,7 @@ in
         # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
         # Only available from driver 515.43.04+
         # Currently alpha-quality/buggy, so false is currently the recommended setting.
-        open = true; # TODO: rtx 2080 is compatible
+        open = true;
 
         # Enable the Nvidia settings menu,
         # accessible via `nvidia-settings`.
