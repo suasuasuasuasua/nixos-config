8f2d29ffcc542ee1c71b1e44d55785819fbd327a
Justin Hoang <j124.dev@proton.me>
2025-04-13 15:17:01 -0600

    docs(flake): remove TODOs for pre-commit split

--- flake.nix
+++ flake.nix
@@ -92,7 +92,6 @@
       overlays = import ./overlays { inherit inputs outputs; };
       formatter = forEachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
 
-      # TODO: should i split the pre-commits into its own file?
       checks = {
         formatting = forEachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.check self);
         git-hooks-check = forEachSystem (
@@ -106,7 +105,6 @@
           }
         );
       };
-      # TODO: should i split this dev shell into its own file?
       devShells = forEachSystem (pkgs: {
         default = import ./shell.nix {
           inherit self pkgs;
