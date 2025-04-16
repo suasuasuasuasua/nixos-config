417ab1a43e2f714a3913d44d4f05f09c823705dc
Justin Hoang <j124.dev@proton.me>
2025-04-16 12:47:00 -0600

    fix(firefox): remove extensions that i don't use

--- modules/home/gui/firefox.nix
+++ modules/home/gui/firefox.nix
@@ -133,12 +133,10 @@ in
         # https://nur.nix-community.org/repos/rycee/
         extensions = with pkgs.firefox-addons; [
           betterttv # twitch [dot] tv integration
-          # bypass-paywalls-clean # WARNING: bypass the paywalls - not available
           darkreader # automatic dark mode
           don-t-fuck-with-paste # prevent websites from modifying copy+paste
           edit-with-emacs # allow emacs-like editing in text fields
           reddit-enhancement-suite # better reddit
-          seventv # another twitch add-on
           ublock-origin # block ads
           unpaywall # read research papers for free
           vimium # vim-like movements
