bcf1955bb334b7c66191a3ca80c5987f6bc71483
Justin Hoang <j124.dev@proton.me>
2025-04-24 21:19:41 -0600

    fix(vscode): switching to vscodium package
    will leave filename for legacy purposes

--- modules/home/gui/vscode.nix
+++ modules/home/gui/vscode.nix
@@ -20,21 +20,43 @@ in
 
     programs.vscode = {
       enable = true;
+      package = pkgs.vscodium; # NOTE: fhs version exists
       extensions = with pkgs.vscode-extensions; [
         # General
-        vscodevim.vim
-        gruntfuggly.todo-tree
-        codezombiech.gitignore
+        aaron-bond.better-comments # colorized comments
+        adpyke.codesnap # screenshot code
+        christian-kohler.path-intellisense # autofill file and folder paths
+        codezombiech.gitignore # gitignore templates
+        gruntfuggly.todo-tree # todo tree view
+        hediet.vscode-drawio # draw.io integration
+        johnpapa.vscode-peacock # colorized projects
+        mikestead.dotenv # [dot] env file integration
+        mkhl.direnv # direnv integration
+        vscodevim.vim # vim emulation
+        waderyan.gitblame # git blame
+
+        # Formatters and Linters
+        davidanson.vscode-markdownlint # Markdown
+        esbenp.prettier-vscode # General
+        ms-python.black-formatter # Python
+        valentjn.vscode-ltex # Spell-check (NOTE: "plus" and harper not in nixpkgs)
+
+        # LSPs and more
+        james-yu.latex-workshop # LaTeX
+        jnoortheen.nix-ide # Nix
+        mads-hartmann.bash-ide-vscode # Bash
+        ms-python.python # Python
+        ms-toolsai.jupyter # Jupyter
+        ms-vscode.cpptools # C/C++
+        myriad-dreamin.tinymist # Typst
+        redhat.java # Java
+        redhat.vscode-yaml # YAML
 
         # Remote
         ms-vscode-remote.remote-ssh
         ms-vscode-remote.remote-ssh-edit
         ms-vscode-remote.remote-wsl
         ms-vscode-remote.remote-containers
-
-        # Nix Dev
-        jnoortheen.nix-ide
-        mkhl.direnv
       ];
 
       # TODO: profiles available in 25.05 unstable...
