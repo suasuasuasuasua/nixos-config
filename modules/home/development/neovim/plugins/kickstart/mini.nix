8789a5f850ab29ba18749cf43360448802278f67
Justin Hoang <j124.dev@proton.me>
2025-04-27 15:28:21 -0600

    fix(nixvim): disable mini starter for now
    just kind of bloat sometimes

--- modules/home/development/neovim/plugins/kickstart/mini.nix
+++ modules/home/development/neovim/plugins/kickstart/mini.nix
@@ -23,41 +23,41 @@
         # gS to split and join (https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-splitjoin.md)
         splitjoin = { };
 
-        starter = {
-          content_hooks = {
-            "__unkeyed-1.adding_bullet" = {
-              __raw = "require('mini.starter').gen_hook.adding_bullet()";
-            };
-            "__unkeyed-2.indexing" = {
-              __raw = "require('mini.starter').gen_hook.indexing('all', { 'Builtin actions' })";
-            };
-            "__unkeyed-3.padding" = {
-              __raw = "require('mini.starter').gen_hook.aligning('center', 'center')";
-            };
-          };
-          evaluate_single = true;
-          header = ''
-            ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗
-            ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║
-            ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║
-            ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║
-            ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║
-          '';
-          items = {
-            "__unkeyed-1.buildtin_actions" = {
-              __raw = "require('mini.starter').sections.builtin_actions()";
-            };
-            "__unkeyed-2.recent_files_current_directory" = {
-              __raw = "require('mini.starter').sections.recent_files(10, true)";
-            };
-            "__unkeyed-3.recent_files" = {
-              __raw = "require('mini.starter').sections.recent_files(10, false)";
-            };
-            "__unkeyed-4.sessions" = {
-              __raw = "require('mini.starter').sections.sessions(5, true)";
-            };
-          };
-        };
+        # starter = {
+        #   content_hooks = {
+        #     "__unkeyed-1.adding_bullet" = {
+        #       __raw = "require('mini.starter').gen_hook.adding_bullet()";
+        #     };
+        #     "__unkeyed-2.indexing" = {
+        #       __raw = "require('mini.starter').gen_hook.indexing('all', { 'Builtin actions' })";
+        #     };
+        #     "__unkeyed-3.padding" = {
+        #       __raw = "require('mini.starter').gen_hook.aligning('center', 'center')";
+        #     };
+        #   };
+        #   evaluate_single = true;
+        #   header = ''
+        #     ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗
+        #     ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║
+        #     ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║
+        #     ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║
+        #     ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+        #   '';
+        #   items = {
+        #     "__unkeyed-1.buildtin_actions" = {
+        #       __raw = "require('mini.starter').sections.builtin_actions()";
+        #     };
+        #     "__unkeyed-2.recent_files_current_directory" = {
+        #       __raw = "require('mini.starter').sections.recent_files(10, true)";
+        #     };
+        #     "__unkeyed-3.recent_files" = {
+        #       __raw = "require('mini.starter').sections.recent_files(10, false)";
+        #     };
+        #     "__unkeyed-4.sessions" = {
+        #       __raw = "require('mini.starter').sections.sessions(5, true)";
+        #     };
+        #   };
+        # };
 
         # ... and there is more!
         # Check out: https://github.com/echasnovski/mini.nvim
