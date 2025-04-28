d7e57932e6b1a528467b70d9468cf6969227de79
Justin Hoang <j124.dev@proton.me>
2025-04-27 22:32:19 -0600

    build(flake): added home-manager unstable and cleaned up inputs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   description = "suasuasuasuasua's nixos configuration";
 
   inputs = {
-    # Principle inputs (updated by `nix run .#update`)
-    # packages
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-darwin = {
@@ -21,43 +19,42 @@
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
-    mac-app-util.url = "github:hraban/mac-app-util";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11"; # head
-      # url = "github:nix-community/home-manager"; # unstable
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager-unstable = {
+      url = "github:nix-community/home-manager"; # unstable
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # desktop
+    # utility
+    disko.url = "github:nix-community/disko/latest";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    rpi-nix.url = "github:nix-community/raspberry-pi-nix";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+
+    # software inputs
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-
-    # utility
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    disko.url = "github:nix-community/disko/latest";
-    rpi-nix.url = "github:nix-community/raspberry-pi-nix";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    git-hooks-nix.url = "github:cachix/git-hooks.nix";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
-
-    # software inputs
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
-    # extras
-    catppuccin.url = "github:catppuccin/nix";
+    # apps
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
