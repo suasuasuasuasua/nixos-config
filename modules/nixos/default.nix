79acaf78a35b961190f09fafb5485176ff109e0a
Justin Hoang <j124.dev@proton.me>
2025-05-03 14:44:36 -0600

    refactor(nixos): rewrote system packages more idiomatically

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -11,9 +11,17 @@
   # write a list of system packages to /etc/current-system-packages
   environment.etc."current-system-packages".text =
     let
-      packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
-      sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
-      formatted = builtins.concatStringsSep "\n" sortedUnique;
+      inherit (builtins)
+        lessThan
+        map
+        sort
+        concatStringsSep
+        ;
+      inherit (pkgs.lib.lists) unique;
+
+      packages = map (p: "${p.name}") config.environment.systemPackages;
+      sortedUnique = sort lessThan (unique packages);
+      formatted = concatStringsSep "\n" sortedUnique;
     in
     formatted;
 
