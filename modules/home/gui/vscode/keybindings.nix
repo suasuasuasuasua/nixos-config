cb674f1c5765e538e73b3058e4e21cb0e994df3d
Justin Hoang <j124.dev@proton.me>
2025-05-04 15:05:34 -0600

    refactor(vscode): condensed the keybindings with modifier keys
    names are sussy but it works

--- modules/home/gui/vscode/keybindings.nix
+++ modules/home/gui/vscode/keybindings.nix
@@ -4,26 +4,24 @@
   keybindingSubmodule,
   ...
 }:
-# define the types and inherits
-let
-  inherit (lib) optionals;
-  inherit (pkgs.stdenv) isDarwin isLinux;
+lib.mkOption {
+  type = keybindingSubmodule;
+  default =
+    let
+      inherit (pkgs.stdenv) isDarwin;
 
-  bottomPanelBindings =
-    optionals isDarwin [
+      mod_ctrl = if isDarwin then "cmd" else "ctrl";
+      mod_alt = if isDarwin then "cmd" else "alt";
+    in
+    # bottom panel bindings
+    [
       {
-        key = "ctrl+cmd+j";
+        key = "ctrl+${mod_ctrl}+j";
         command = "workbench.action.togglePanel";
       }
     ]
-    ++ optionals isLinux [
-      {
-        key = "ctrl+alt+j";
-        command = "workbench.action.togglePanel";
-      }
-    ];
-  editorManipulationBindings =
-    [
+    # editor manipulation bindings
+    ++ [
       {
         key = "ctrl+y";
         command = "acceptSelectedSuggestion";
@@ -47,189 +45,97 @@ let
         key = "ctrl+alt+l";
         command = "workbench.action.moveEditorRightInGroup";
       }
-    ]
-    ++ optionals isDarwin [
       {
-        key = "cmd+4";
+        key = "${mod_ctrl}+4";
         command = "workbench.action.closeActiveEditor";
       }
       {
-        key = "cmd+w";
+        key = "${mod_ctrl}+w";
         command = "-workbench.action.closeActiveEditor";
       }
       {
-        key = "shift+cmd+b";
+        key = "shift+${mod_ctrl}+b";
         command = "workbench.action.toggleAuxiliaryBar";
       }
       {
-        key = "shift+cmd+a";
+        key = "shift+${mod_ctrl}+a";
         command = "workbench.action.toggleActivityBarVisibility";
       }
       {
-        key = "shift+cmd+=";
+        key = "shift+${mod_ctrl}+=";
         command = "-workbench.action.zoomIn";
       }
       {
-        key = "shift+cmd+-";
+        key = "shift+${mod_ctrl}+-";
         command = "-workbench.action.zoomOut";
       }
       {
-        key = "cmd+0";
+        key = "${mod_ctrl}+0";
         command = "workbench.action.zoomReset";
       }
       {
-        key = "cmd+numpad0";
+        key = "${mod_ctrl}+numpad0";
         command = "-workbench.action.zoomReset";
       }
       {
-        key = "shift+cmd+j";
+        key = "shift+${mod_ctrl}+j";
         command = "workbench.action.moveActiveEditorGroupDown";
       }
       {
-        key = "cmd+k down";
+        key = "${mod_ctrl}+k down";
         command = "-workbench.action.moveActiveEditorGroupDown";
       }
       {
-        key = "shift+cmd+h";
+        key = "shift+${mod_ctrl}+h";
         command = "workbench.action.moveActiveEditorGroupLeft";
       }
       {
-        key = "cmd+k left";
+        key = "${mod_ctrl}+k left";
         command = "-workbench.action.moveActiveEditorGroupLeft";
       }
       {
-        key = "shift+cmd+l";
+        key = "shift+${mod_ctrl}+l";
         command = "workbench.action.moveActiveEditorGroupRight";
       }
       {
-        key = "cmd+k right";
+        key = "${mod_ctrl}+k right";
         command = "-workbench.action.moveActiveEditorGroupRight";
       }
       {
-        key = "shift+cmd+k";
+        key = "shift+${mod_ctrl}+k";
         command = "workbench.action.moveActiveEditorGroupUp";
       }
       {
-        key = "cmd+k up";
+        key = "${mod_ctrl}+k up";
         command = "-workbench.action.moveActiveEditorGroupUp";
       }
       {
-        key = "ctrl+shift+cmd+k";
+        key = "ctrl+shift+${mod_alt}+k";
         command = "workbench.action.moveEditorToAboveGroup";
       }
       {
-        key = "ctrl+shift+cmd+j";
+        key = "ctrl+shift+${mod_alt}+j";
         command = "workbench.action.moveEditorToBelowGroup";
       }
       {
-        key = "ctrl+shift+cmd+h";
+        key = "ctrl+shift+${mod_alt}+h";
         command = "workbench.action.moveEditorToLeftGroup";
       }
       {
-        key = "ctrl+shift+cmd+l";
+        key = "ctrl+shift+${mod_alt}+l";
         command = "workbench.action.moveEditorToRightGroup";
       }
       {
-        key = "cmd+k shift+cmd+left";
+        key = "${mod_ctrl}+k shift+${mod_ctrl}+left";
         command = "-workbench.action.moveEditorLeftInGroup";
       }
       {
-        key = "cmd+k shift+cmd+right";
+        key = "${mod_ctrl}+k shift+${mod_ctrl}+right";
         command = "-workbench.action.moveEditorRightInGroup";
       }
     ]
-    ++ optionals isLinux [
-      {
-        key = "ctrl+4";
-        command = "workbench.action.closeActiveEditor";
-      }
-      {
-        key = "ctrl+w";
-        command = "-workbench.action.closeActiveEditor";
-      }
-      {
-        key = "shift+ctrl+b";
-        command = "workbench.action.toggleAuxiliaryBar";
-      }
-      {
-        key = "shift+ctrl+a";
-        command = "workbench.action.toggleActivityBarVisibility";
-      }
-      {
-        key = "shift+ctrl+=";
-        command = "-workbench.action.zoomIn";
-      }
-      {
-        key = "shift+ctrl+-";
-        command = "-workbench.action.zoomOut";
-      }
-      {
-        key = "ctrl+0";
-        command = "workbench.action.zoomReset";
-      }
-      {
-        key = "ctrl+numpad0";
-        command = "-workbench.action.zoomReset";
-      }
-      {
-        key = "shift+ctrl+j";
-        command = "workbench.action.moveActiveEditorGroupDown";
-      }
-      {
-        key = "ctrl+k down";
-        command = "-workbench.action.moveActiveEditorGroupDown";
-      }
-      {
-        key = "shift+ctrl+h";
-        command = "workbench.action.moveActiveEditorGroupLeft";
-      }
-      {
-        key = "ctrl+k left";
-        command = "-workbench.action.moveActiveEditorGroupLeft";
-      }
-      {
-        key = "shift+ctrl+l";
-        command = "workbench.action.moveActiveEditorGroupRight";
-      }
-      {
-        key = "ctrl+k right";
-        command = "-workbench.action.moveActiveEditorGroupRight";
-      }
-      {
-        key = "shift+ctrl+k";
-        command = "workbench.action.moveActiveEditorGroupUp";
-      }
-      {
-        key = "ctrl+k up";
-        command = "-workbench.action.moveActiveEditorGroupUp";
-      }
-      {
-        key = "ctrl+shift+alt+k";
-        command = "workbench.action.moveEditorToAboveGroup";
-      }
-      {
-        key = "ctrl+shift+alt+j";
-        command = "workbench.action.moveEditorToBelowGroup";
-      }
-      {
-        key = "ctrl+shift+alt+h";
-        command = "workbench.action.moveEditorToLeftGroup";
-      }
-      {
-        key = "ctrl+shift+alt+l";
-        command = "workbench.action.moveEditorToRightGroup";
-      }
-      {
-        key = "ctrl+k shift+ctrl+left";
-        command = "-workbench.action.moveEditorLeftInGroup";
-      }
-      {
-        key = "ctrl+k shift+ctrl+right";
-        command = "-workbench.action.moveEditorRightInGroup";
-      }
-    ];
-  editorNavigationBindings =
-    [
+    # editor navigation bindings
+    ++ [
       {
         key = "alt+h";
         command = "workbench.action.previousEditorInGroup";
@@ -252,109 +158,56 @@ let
         "command" = "workbench.action.quickOpenSelectPrevious";
         "when" = "inQuickOpen";
       }
-    ]
-    ++ optionals isDarwin [
       {
-        key = "cmd+h";
+        key = "${mod_ctrl}+h";
         command = "workbench.action.focusLeftGroup";
       }
       {
-        key = "cmd+k cmd+left";
+        key = "${mod_ctrl}+k ${mod_ctrl}+left";
         command = "-workbench.action.focusLeftGroup";
       }
       {
-        key = "cmd+l";
+        key = "${mod_ctrl}+l";
         command = "workbench.action.focusRightGroup";
       }
       {
-        key = "cmd+k cmd+right";
+        key = "${mod_ctrl}+k ${mod_ctrl}+right";
         command = "-workbench.action.focusRightGroup";
       }
       {
-        key = "cmd+k";
+        key = "${mod_ctrl}+k";
         command = "workbench.action.focusAboveGroup";
       }
       {
-        key = "cmd+k cmd+up";
+        key = "${mod_ctrl}+k ${mod_ctrl}+up";
         command = "-workbench.action.focusAboveGroup";
       }
       {
-        key = "cmd+j";
+        key = "${mod_ctrl}+j";
         command = "workbench.action.focusBelowGroup";
       }
       {
-        key = "cmd+k cmd+down";
+        key = "${mod_ctrl}+k ${mod_ctrl}+down";
         command = "-workbench.action.focusBelowGroup";
       }
       {
-        key = "cmd+k alt+cmd+left";
+        key = "${mod_ctrl}+k alt+${mod_ctrl}+left";
         command = "-workbench.action.previousEditorInGroup";
       }
       {
-        key = "cmd+k alt+cmd+right";
+        key = "${mod_ctrl}+k alt+${mod_ctrl}+right";
         command = "-workbench.action.nextEditorInGroup";
       }
       {
-        key = "ctrl+cmd+e";
+        key = "ctrl+${mod_alt}+e";
         command = "workbench.action.focusActiveEditorGroup";
       }
       {
-        key = "ctrl+cmd+b";
+        key = "ctrl+${mod_alt}+b";
         command = "workbench.action.focusSideBar";
       }
     ]
-    ++ optionals isLinux [
-      {
-        key = "ctrl+h";
-        command = "workbench.action.focusLeftGroup";
-      }
-      {
-        key = "ctrl+k ctrl+left";
-        command = "-workbench.action.focusLeftGroup";
-      }
-      {
-        key = "ctrl+l";
-        command = "workbench.action.focusRightGroup";
-      }
-      {
-        key = "ctrl+k ctrl+right";
-        command = "-workbench.action.focusRightGroup";
-      }
-      {
-        key = "ctrl+k";
-        command = "workbench.action.focusAboveGroup";
-      }
-      {
-        key = "ctrl+k ctrl+up";
-        command = "-workbench.action.focusAboveGroup";
-      }
-      {
-        key = "ctrl+j";
-        command = "workbench.action.focusBelowGroup";
-      }
-      {
-        key = "ctrl+k ctrl+down";
-        command = "-workbench.action.focusBelowGroup";
-      }
-      {
-        key = "ctrl+k alt+ctrl+left";
-        command = "-workbench.action.previousEditorInGroup";
-      }
-      {
-        key = "ctrl+k alt+ctrl+right";
-        command = "-workbench.action.nextEditorInGroup";
-      }
-      {
-        key = "ctrl+alt+e";
-        command = "workbench.action.focusActiveEditorGroup";
-      }
-      {
-        key = "ctrl+alt+b";
-        command = "workbench.action.focusSideBar";
-      }
-    ];
-  explorerBindings =
-    [
+    ++ [
       {
         key = "shift+j";
         command = "list.expandSelectionDown";
@@ -375,146 +228,78 @@ let
         command = "-list.expandSelectionUp";
         when = "listFocus && listSupportsMultiselect && !inputFocus";
       }
-    ]
-    ++ optionals isDarwin [
       {
-        key = "shift+cmd+c";
+        key = "shift+${mod_ctrl}+c";
         command = "workbench.files.action.collapseExplorerFolders";
       }
     ]
-    ++ optionals isLinux [
+    # finder keybindings
+    ++ [
       {
-        key = "shift+ctrl+c";
-        command = "workbench.files.action.collapseExplorerFolders";
-      }
-    ];
-  finderBindings =
-    optionals isDarwin [
-      {
-        key = "shift+cmd+f";
+        key = "shift+${mod_ctrl}+f";
         command = "-workbench.action.findInFiles";
       }
       {
-        key = "shift+cmd+f";
+        key = "shift+${mod_ctrl}+f";
         command = "-workbench.action.terminal.searchWorkspace";
         when = "terminalFocus && terminalProcessSupported && terminalTextSelected";
       }
       {
-        key = "shift+cmd+f";
+        key = "shift+${mod_ctrl}+f";
         command = "-workbench.view.search";
         when = "workbench.view.search.active && neverMatch =~ /doesNotMatch/";
       }
       {
-        key = "shift+cmd+f";
+        key = "shift+${mod_ctrl}+f";
         command = "workbench.view.search.focus";
       }
     ]
-    ++ optionals isLinux [
+    # previewer bindings
+    ++ [
       {
-        key = "shift+ctrl+f";
-        command = "-workbench.action.findInFiles";
-      }
-      {
-        key = "shift+ctrl+f";
-        command = "-workbench.action.terminal.searchWorkspace";
-        when = "terminalFocus && terminalProcessSupported && terminalTextSelected";
-      }
-      {
-        key = "shift+ctrl+f";
-        command = "-workbench.view.search";
-        when = "workbench.view.search.active && neverMatch =~ /doesNotMatch/";
-      }
-      {
-        key = "shift+ctrl+f";
-        command = "workbench.view.search.focus";
-      }
-    ];
-  previewerBindings =
-    optionals isDarwin [
-      {
-        key = "ctrl+cmd+v";
+        key = "ctrl+${mod_alt}+v";
         command = "livePreview.start.internalPreview.atFile";
         when = "editorLangId == 'html'";
       }
     ]
-    ++ optionals isLinux [
+    # source control bindings
+    ++ [
       {
-        key = "ctrl+alt+v";
-        command = "livePreview.start.internalPreview.atFile";
-        when = "editorLangId == 'html'";
-      }
-    ];
-  sourceControlBindings =
-    optionals isDarwin [
-      {
-        key = "shift+cmd+g";
+        key = "shift+${mod_ctrl}+g";
         command = "workbench.view.scm";
         when = "workbench.scm.active";
       }
       {
-        key = "shift+cmd+g";
+        key = "shift+${mod_ctrl}+g";
         command = "-workbench.view.scm";
         when = "workbench.scm.active";
       }
     ]
-    ++ optionals isLinux [
-      {
-        key = "shift+ctrl+g";
-        command = "workbench.view.scm";
-        when = "workbench.scm.active";
-      }
-      {
-        key = "shift+ctrl+g";
-        command = "-workbench.view.scm";
-        when = "workbench.scm.active";
-      }
-    ];
-  terminalBindings =
-    [
+    # terminal bindings
+    ++ [
       {
         key = "ctrl+`";
         command = "-workbench.action.terminal.toggleTerminal";
         when = "terminal.active";
 
       }
-    ]
-    ++ optionals isDarwin [
       {
-        key = "ctrl+cmd+k";
+        key = "ctrl+${mod_alt}+k";
         command = "workbench.action.terminal.toggleTerminal";
         when = "terminal.active";
       }
     ]
-    ++ optionals isLinux [
+    # vim bindings
+    ++ [
       {
-        key = "ctrl+alt+k";
-        command = "workbench.action.terminal.toggleTerminal";
-        when = "terminal.active";
+        key = "ctrl+h";
+        command = "-extension.vim_ctrl+h";
+        when = "editorTextFocus && vim.active && vim.use<C-h> && !inDebugRepl";
+      }
+      {
+        key = "ctrl+l";
+        command = "-extension.vim_navigateCtrlL";
+        when = "editorTextFocus && vim.active && vim.use<C-l> && !inDebugRepl";
       }
     ];
-  vimBindings = [
-    {
-      key = "ctrl+h";
-      command = "-extension.vim_ctrl+h";
-      when = "editorTextFocus && vim.active && vim.use<C-h> && !inDebugRepl";
-    }
-    {
-      key = "ctrl+l";
-      command = "-extension.vim_navigateCtrlL";
-      when = "editorTextFocus && vim.active && vim.use<C-l> && !inDebugRepl";
-    }
-  ];
-in
-lib.mkOption {
-  type = keybindingSubmodule;
-  default =
-    bottomPanelBindings
-    ++ editorManipulationBindings
-    ++ editorNavigationBindings
-    ++ explorerBindings
-    ++ finderBindings
-    ++ previewerBindings
-    ++ sourceControlBindings
-    ++ terminalBindings
-    ++ vimBindings;
 }
