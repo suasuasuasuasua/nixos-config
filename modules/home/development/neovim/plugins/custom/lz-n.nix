735adc2c9a027a828dd6380c14daa2bf00f59d1a
Justin Hoang <j124.dev@proton.me>
2025-05-05 00:21:07 -0600

    chore(nixvim): copied scaffolding for neo-tree and telescope
    seems to work?

--- modules/home/development/neovim/plugins/custom/lz-n.nix
+++ modules/home/development/neovim/plugins/custom/lz-n.nix
@@ -22,6 +22,50 @@ in
       plugins.lz-n = {
         enable = true;
 
+        # https://nix-community.github.io/nixvim/plugins/lz-n/plugins.html
+        plugins = [
+          # neotree
+          {
+            __unkeyed-1 = "neo-tree.nvim"; # the plugin's name (:h packadd)
+            after = ''
+              function()
+                require("neo-tree").setup()
+              end
+            '';
+            enabled = ''
+              function()
+              return true
+              end
+            '';
+            keys = [
+              {
+                __unkeyed-1 = "<leader>ft";
+                __unkeyed-2 = "<CMD>Neotree toggle<CR>";
+                desc = "NeoTree toggle";
+              }
+            ];
+          }
+          # telescope
+          {
+            __unkeyed-1 = "telescope.nvim";
+            cmd = [
+              "Telescope"
+            ];
+            keys = [
+              {
+                __unkeyed-1 = "<leader>fa";
+                __unkeyed-2 = "<CMD>Telescope autocommands<CR>";
+                desc = "Telescope autocommands";
+              }
+              {
+                __unkeyed-1 = "<leader>fb";
+                __unkeyed-2 = "<CMD>Telescope buffers<CR>";
+                desc = "Telescope buffers";
+              }
+            ];
+          }
+        ];
+
         # :h autocmd-events
         settings = { };
       };
