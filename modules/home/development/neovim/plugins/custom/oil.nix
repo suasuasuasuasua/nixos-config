6e58597fbae696e02af678b588e7abeffbb2d100
Justin Hoang <j124.dev@proton.me>
2025-05-04 23:27:50 -0600

    feat(nixvim): added oil module

---
+++ modules/home/development/neovim/plugins/custom/oil.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  name = "oil";
+  cfg = config.home.development.neovim.plugins.${name};
+in
+{
+  options.home.development.neovim.plugins.${name} = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable ${name} plugin for neovim";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.nixvim = {
+      # https://github.com/stevearc/oil.nvim
+      plugins.oil = {
+        enable = true;
+
+        settings = {
+          # Id is automatically added at the beginning, and name at the end
+          # Frr :uryc bvy-pbyhzaf
+          pbyhzaf = [
+            "icon"
+            "crezvffvbaf"
+            "fvmr"
+            "mtime"
+          ];
+
+          # Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+          delete_to_trash = false;
+        };
+      };
+    };
+  };
+}
