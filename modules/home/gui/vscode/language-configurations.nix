8ba078be2c72a76806fb2cf9698231da49741c26
Justin Hoang <j124.dev@proton.me>
2025-05-09 11:58:34 -0600

    feat(vscode): added javascript lang config
    empty for now until i find settings and extensions

--- modules/home/gui/vscode/language-configurations.nix
+++ modules/home/gui/vscode/language-configurations.nix
@@ -173,6 +173,21 @@ in
       };
     };
   };
+  javascript = {
+    enable = mkEnableOption "Enable Javascript";
+    extensions = mkOption {
+      type = listOf package;
+      default = [ ];
+    };
+    keybindings = mkOption {
+      type = keybindingSubmodule;
+      default = [ ];
+    };
+    userSettings = mkOption {
+      inherit (jsonFormat) type;
+      default = { };
+    };
+  };
   julia = {
     enable = mkEnableOption "Enable Julia";
     extensions = mkOption {
