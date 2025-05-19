20f8df4ad07aee32d954a7147990cad0b7f32c14
Justin Hoang <j124.dev@proton.me>
2025-05-19 16:58:37 -0600

    fix(penguin): update config for syncthing

--- configurations/nixos/penguin/services/services.nix
+++ configurations/nixos/penguin/services/services.nix
@@ -6,6 +6,12 @@
   config.nixos.services = {
     syncthing = {
       enable = true;
+      # TODO: kind of hardcoded but only way I could figure out how to
+      # synchronize the home directory
+      dataDir = "/home/justinhoang";
+      group = "users";
+      user = "justinhoang";
+
       settings = import ./syncthing.nix;
     };
   };
