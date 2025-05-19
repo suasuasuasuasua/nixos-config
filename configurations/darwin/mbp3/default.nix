121d81253ed1206183ddc538b1a1dc3bbceea350
Justin Hoang <j124.dev@proton.me>
2025-05-19 08:25:46 -0600

    fix(mbp3): nerd fonts available under pkgs

--- configurations/darwin/mbp3/default.nix
+++ configurations/darwin/mbp3/default.nix
@@ -88,23 +88,23 @@
 
   # Fonts
   fonts = {
-    packages = with pkgs; [
-      # icon fonts
-      material-design-icons
-      font-awesome
-      # nerdfonts
-      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
-      (nerdfonts.override {
-        fonts = [
-          # symbols icon only
-          "NerdFontsSymbolsOnly"
-          # Characters
-          "FiraCode"
-          "JetBrainsMono"
-          "Iosevka"
-        ];
-      })
-    ];
+    packages =
+      with pkgs;
+      [
+        # icon fonts
+        material-design-icons
+        font-awesome
+      ]
+      ++ (with pkgs.nerd-fonts; [
+        # nerdfonts
+        # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
+        # symbols icon only
+        symbols-only
+        # characters
+        fira-code
+        jetbrains-mono
+        iosevka
+      ]);
   };
 
   # Set your time zone.
