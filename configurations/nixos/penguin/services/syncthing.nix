36f7d1551d78e980c42221f7d66ba8a7ce4db43c
Justin Hoang <j124.dev@proton.me>
2025-05-20 12:10:21 -0600

    feat(syncthing): added folder for penguin

--- configurations/nixos/penguin/services/syncthing.nix
+++ configurations/nixos/penguin/services/syncthing.nix
@@ -7,4 +7,18 @@
       id = "V4NCONK-S2JLQLH-AQULXDL-Y4QQWBB-FO67NEP-G2Q2SSG-BQF4ZPW-5VHRGAY";
     };
   };
+  folders = {
+    "notes" = {
+      path = "/home/justinhoang/Documents/vaults/personal";
+      devices = [
+        "lab"
+        "mbp3"
+      ];
+      versioning = {
+        type = "simple";
+        params.keep = "5";
+      };
+      type = "sendreceive";
+    };
+  };
 }
