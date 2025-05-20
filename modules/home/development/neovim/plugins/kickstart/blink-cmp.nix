4d9c0e0bdc092033d58f2313a6e282f3c873b7fc
Justin Hoang <j124.dev@proton.me>
2025-05-20 17:36:47 -0600

    feat(nixvim): added ripgrep for blink cmp

--- modules/home/development/neovim/plugins/kickstart/blink-cmp.nix
+++ modules/home/development/neovim/plugins/kickstart/blink-cmp.nix
@@ -52,6 +52,7 @@
               "buffer"
               "emoji"
               "git"
+              "ripgrep"
             ];
             providers = {
               # TODO: the pop-up for blink-emoji is _really_ slow for some
@@ -96,6 +97,25 @@
                   };
                 };
               };
+              ripgrep = {
+                async = true;
+                module = "blink-ripgrep";
+                name = "Ripgrep";
+                score_offset = 100;
+                opts = {
+                  prefix_min_len = 3;
+                  context_size = 5;
+                  max_filesize = "1M";
+                  project_root_marker = ".git";
+                  project_root_fallback = true;
+                  search_casing = "--ignore-case";
+                  additional_rg_options = { };
+                  fallback_to_regex_highlighting = true;
+                  ignore_paths = { };
+                  additional_paths = { };
+                  debug = false;
+                };
+              };
             };
           };
           snippets = {
@@ -121,6 +141,8 @@
       # Completion sources
       blink-emoji.enable = true;
       blink-cmp-git.enable = true;
+      blink-ripgrep.enable = true;
+
       # Dependencies
       #
       # Snippet Engine & its associated nvim-cmp source
