3ee812126a201dbe402ae2227719802c81a393ca
Justin Hoang <j124.dev@proton.me>
2025-05-21 10:37:07 -0600

    fix(nixvim): fully disable ripgrep integration for blink

--- modules/home/development/neovim/plugins/kickstart/blink-cmp.nix
+++ modules/home/development/neovim/plugins/kickstart/blink-cmp.nix
@@ -97,25 +97,25 @@
                   };
                 };
               };
-              ripgrep = {
-                async = true;
-                module = "blink-ripgrep";
-                name = "Ripgrep";
-                score_offset = 0;
-                opts = {
-                  prefix_min_len = 3;
-                  context_size = 5;
-                  max_filesize = "1M";
-                  project_root_marker = ".git";
-                  project_root_fallback = true;
-                  search_casing = "--ignore-case";
-                  additional_rg_options = { };
-                  fallback_to_regex_highlighting = true;
-                  ignore_paths = { };
-                  additional_paths = { };
-                  debug = false;
-                };
-              };
+              # ripgrep = {
+              #   async = true;
+              #   module = "blink-ripgrep";
+              #   name = "Ripgrep";
+              #   score_offset = 0;
+              #   opts = {
+              #     prefix_min_len = 3;
+              #     context_size = 5;
+              #     max_filesize = "1M";
+              #     project_root_marker = ".git";
+              #     project_root_fallback = true;
+              #     search_casing = "--ignore-case";
+              #     additional_rg_options = { };
+              #     fallback_to_regex_highlighting = true;
+              #     ignore_paths = { };
+              #     additional_paths = { };
+              #     debug = false;
+              #   };
+              # };
             };
           };
           snippets = {
