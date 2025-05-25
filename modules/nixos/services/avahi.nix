48aee8077e0461d418a62aee700bb19c808cf504
Justin Hoang <j124.dev@proton.me>
2025-05-25 16:32:34 -0600

    feat(services): added avahi module

---
+++ modules/nixos/services/avahi.nix
@@ -0,0 +1,28 @@
+{ config, lib, ... }:
+let
+  serviceName = "avahi";
+
+  cfg = config.nixos.services.${serviceName};
+in
+{
+  options.nixos.services.${serviceName} = {
+    enable = lib.mkEnableOption ''
+      mDNS/DNS-SD implementation
+    '';
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.avahi = {
+      enable = true;
+      nssmdns = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        domain = true;
+        hinfo = true;
+        userServices = true;
+        workstation = true;
+      };
+    };
+  };
+}
