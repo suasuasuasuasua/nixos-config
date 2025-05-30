aa8f0ebe8b805b9208c4fd9cd40d8be54ea381e4
Justin Hoang <j124.dev@proton.me>
2025-05-30 12:03:23 -0600

    feat(nginx): added ip route to root path

--- modules/nixos/services/nginx.nix
+++ modules/nixos/services/nginx.nix
@@ -18,5 +18,19 @@ in
       recommendedProxySettings = true;
       recommendedOptimisation = true;
     };
+
+    services.nginx.virtualHosts = {
+      # expose the ip address of the machine
+      "127.0.0.1" = {
+        locations."/ip" = {
+          extraConfig = ''
+            add_header Content-Type "application/json";
+          '';
+          return = ''
+            200 '{"host":"$server_name","ip":"$remote_addr","port":"$remote_port","server_ip":"$server_addr","server_port":"$server_port"}\n'
+          '';
+        };
+      };
+    };
   };
 }
