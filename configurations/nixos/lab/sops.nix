68545b2ab2a87f255c0b43d83ffa9867fe5707e9
Justin Hoang <j124.dev@proton.me>
2025-06-05 22:56:13 -0600

    fix(sops): added slash in the keyFile
    not sure how this was working before lol

--- configurations/nixos/lab/sops.nix
+++ configurations/nixos/lab/sops.nix
@@ -12,7 +12,7 @@
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
       # This is using an age key that is expected to already be in the filesystem
-      keyFile = "${config.users.users.justinhoang.home}.config/sops/age/keys.txt";
+      keyFile = "${config.users.users.justinhoang.home}/.config/sops/age/keys.txt";
 
       # This will generate a new key if the key specified above does not exist
       generateKey = true;
