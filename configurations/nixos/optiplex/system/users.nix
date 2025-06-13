97ce49bd0c8e8bdbad534a0761e7d59149f50d65
Justin Hoang <j124.dev@proton.me>
2025-06-13 15:58:46 -0600

    fix(nixos): added keys for termius iphone user

--- configurations/nixos/optiplex/system/users.nix
+++ configurations/nixos/optiplex/system/users.nix
@@ -10,6 +10,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBBse2Ikd1n7K9MnQiXmC4kNdNOasAVBbgH01pozcsbm justinhoang@Justins-MacBook-Pro.local"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP5vx5WQe2m2fXDFhjnNeYYrY6OIR0y5X0nfdAAlchcl justinhoang@lab"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA12qTb88TMH/x1T2xST2kEviP+RGGQkv+EJFWPboxuv justinhoang@iphone15"
     ];
   };
 }
