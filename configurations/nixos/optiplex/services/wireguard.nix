87680a6e8e2a452159f4cbc2e671c344913cda57
Justin Hoang <j124.dev@proton.me>
2025-06-13 15:07:08 -0600

    fix(optiplex): remove server config and added lab as client

--- configurations/nixos/optiplex/services/wireguard.nix
+++ configurations/nixos/optiplex/services/wireguard.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
   # "wg0" is the network interface name. You can name the interface arbitrarily.
   wg0 = {
@@ -7,23 +7,6 @@
 
     # The port that WireGuard listens to. Must be accessible by the client.
     listenPort = 51820;
-
-    # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-    # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-    # ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT;
-    postSetup =
-      # bash
-      ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp4s0 -j MASQUERADE
-      '';
-
-    # This undoes the above command
-    # ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT;
-    postShutdown =
-      # bash
-      ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp4s0 -j MASQUERADE
-      '';
 
     # https://superuser.com/questions/1716478/wireguard-no-access-to-the-internet
 
@@ -41,8 +24,18 @@
         name = "lab";
         # Public key of the peer (not a file path).
         publicKey = "JVBP0NWpR70JT0bUoFsunFkGT9YZSY8O/UeMdUxAXlU=";
+
         # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-        allowedIPs = [ "10.100.0.1/32" ];
+        allowedIPs = [
+          "::/0"
+          "0.0.0.0/0"
+          "192.168.0.0/24"
+          "10.100.0.0/24"
+        ];
+
+        # Set this to the server IP and port.
+        endpoint = "192.168.0.240:51820";
+        # endpoint = "vpn-sua.duckdns.org:51820";
       }
     ];
   };
