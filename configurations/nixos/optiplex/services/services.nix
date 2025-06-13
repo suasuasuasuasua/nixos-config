b2bfad08cdfa6b1f0680c089e1dd56227baffcc4
Justin Hoang <j124.dev@proton.me>
2025-06-13 11:35:56 -0600

    fix(optiplex): remove unnecessary services for backup server

--- configurations/nixos/optiplex/services/services.nix
+++ configurations/nixos/optiplex/services/services.nix
@@ -8,16 +8,16 @@ let
   inherit (config.networking) hostName domain;
 in
 {
-  imports = [ "${inputs.self}/modules/nixos/services" ];
+  imports = [
+    # import the modules
+    "${inputs.self}/modules/nixos/services"
+  ];
 
   # This is the actual specification of the secrets.
   sops.secrets = {
     "acme/namecheap_api" = { };
     "duckdns/domains" = { };
     "duckdns/token" = { };
-    "firefox-syncserver/token" = { };
-    "miniflux/credentials" = { };
-    "vaultwarden/environmentFile" = { };
     "wireguard/private_key" = {
       sopsFile = "${inputs.self}/configurations/nixos/lab/secrets.yaml";
     };
@@ -29,15 +29,8 @@ in
       enable = true;
       environmentFile = config.sops.secrets."acme/namecheap_api".path;
     };
-    actual.enable = true;
     adguardhome.enable = true;
-    audiobookshelf.enable = true;
     avahi.enable = true;
-    calibre = {
-      enable = true;
-      libraries = [ "/zshare/media/books/ebooks/" ];
-    };
-    code-server.enable = true;
     dashy = {
       enable = true;
       settings = import ./dashy.nix {
@@ -49,63 +42,15 @@ in
       domainsFile = config.sops.secrets."duckdns/domains".path;
       tokenFile = config.sops.secrets."duckdns/token".path;
     };
-    fail2ban.enable = true;
-    firefox-syncserver = {
-      enable = true;
-      secrets = config.sops.secrets."firefox-syncserver/token".path;
-    };
-    gitweb = {
-      enable = true;
-      projectroot = "/zshare/srv/git";
-    };
     glances.enable = true;
-    immich = {
-      enable = true;
-      mediaLocation = "/zshare/personal/images";
-    };
-    jellyfin.enable = true;
-    jellyseerr.enable = true;
-    mealie.enable = true;
-    miniflux = {
-      enable = true;
-      adminCredentialsFile = config.sops.secrets."miniflux/credentials".path;
-    };
-    navidrome = {
-      enable = true;
-      MusicFolder = "/zshare/media/music";
-    };
+    fail2ban.enable = true;
     nginx.enable = true;
-    ollama = {
-      enable = true;
-      acceleration = false; # no gpu sadge
-    };
-    open-webui.enable = true;
-    paperless = {
-      enable = true;
-      mediaDir = "/zshare/personal/docs";
-    };
-    samba = {
-      enable = true;
-      settings = import ./samba.nix;
-    };
-    stirling-pdf.enable = true;
-    syncthing = {
-      enable = true;
-      settings = import ./syncthing.nix;
-    };
     uptime-kuma.enable = true;
-    vaultwarden = {
-      enable = true;
-      environmentFile = config.sops.secrets."vaultwarden/environmentFile".path;
-    };
-    vscode-server.enable = true;
-    wastebin.enable = true;
     wireguard = {
       enable = true;
       interfaces = import ./wireguard.nix {
         inherit config pkgs;
       };
     };
-    ytdl-sub.enable = true;
   };
 }
