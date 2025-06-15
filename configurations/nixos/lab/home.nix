119352865db09ca2e4c5f6b7d6a8211845052720
Justin Hoang <j124.dev@proton.me>
2025-06-15 14:09:22 -0600

    fix(lab): remove stylix config

--- configurations/nixos/lab/home.nix
+++ configurations/nixos/lab/home.nix
@@ -50,19 +50,6 @@ in
 
         packages = [ nixvim ];
       };
-
-      # NOTE: darwin module provides the home manager module
-      # add under each user rather than global under stylix.nix
-      stylix.targets = {
-        firefox = {
-          enable = true;
-          colorTheme.enable = true;
-          profileNames = [
-            "personal"
-            "productivity"
-          ];
-        };
-      };
     };
   };
 }
