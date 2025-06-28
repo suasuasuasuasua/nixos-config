8f32a26ae658ecf381b3065d25375143bb5b83ea
Justin Hoang <j124.dev@proton.me>
2025-06-27 22:32:42 -0600

    chore(home-assistant): testing out home assistant

--- configurations/nixos/lab/services/services.nix
+++ configurations/nixos/lab/services/services.nix
@@ -24,6 +24,47 @@ in
     };
   };
 
+  services.home-assistant = {
+    enable = true;
+    extraComponents = [
+      # Components required to complete the onboarding
+      "analytics"
+      "google_translate"
+      "met"
+      "radio_browser"
+      "shopping_list"
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      "isal"
+    ];
+    config = {
+      http = {
+        server_port = 8123;
+        server_host = "::1";
+        trusted_proxies = [ "::1" ];
+        use_x_forwarded_for = true;
+      };
+
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = { };
+    };
+  };
+  services.nginx.virtualHosts = {
+    "home-assistant.${hostName}.${domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      acmeRoot = null;
+      extraConfig = ''
+        proxy_buffering off;
+      '';
+      locations."/" = {
+        proxyPass = "http://localhost:8123";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
   # services
   nixos.services = {
     acme = {
