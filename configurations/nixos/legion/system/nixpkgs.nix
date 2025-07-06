e98df62cf67f9aa33c4bb871e56f0bc8cef4648d
Justin Hoang <j124.dev@proton.me>
2025-07-06 13:50:13 -0600

    fix(legion): reduce number of unfree predicate

--- configurations/nixos/legion/system/nixpkgs.nix
+++ configurations/nixos/legion/system/nixpkgs.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 {
   nixpkgs = {
     config = {
@@ -18,32 +18,14 @@
           ]
           # nvidia
           ++ [
-            "cuda_cccl"
             "cuda_cudart"
-            "cuda_cuobjdump"
-            "cuda_cupti"
-            "cuda_cuxxfilt"
-            "cuda_gdb"
-            "cuda-merged"
+            "cuda_cccl"
             "cuda_nvcc"
-            "cuda_nvdisasm"
-            "cuda_nvml_dev"
-            "cuda_nvprune"
-            "cuda_nvrtc"
-            "cuda_nvtx"
-            "cuda_profiler_api"
-            "cuda_sanitizer_api"
-            "cudnn"
             "libcublas"
             "libcufft"
-            "libcurand"
-            "libcusolver"
-            "libcusparse"
-            "libcusparse_lt"
             "libnpp"
-            "libnvjitlink"
-            "nvidia-x11"
             "nvidia-settings"
+            "nvidia-x11"
           ]
           # vscode
           ++ [
@@ -59,18 +41,6 @@
         );
     };
 
-    overlays = [
-      # https://github.com/NixOS/nixpkgs/issues/388681
-      # TODO:remove when fixed for legion build (open-webui is broken i think)
-      (_: prev: {
-        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-          (_: python-prev: {
-            onnxruntime = python-prev.onnxruntime.overridePythonAttrs (oldAttrs: {
-              buildInputs = lib.lists.remove pkgs.onnxruntime oldAttrs.buildInputs;
-            });
-          })
-        ];
-      })
-    ];
+    overlays = [ ];
   };
 }
