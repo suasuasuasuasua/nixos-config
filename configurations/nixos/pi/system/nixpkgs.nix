84fb95acc1385e983936c9fdd224e2a95cd83cf9
Justin Hoang <j124.dev@proton.me>
2025-07-10 21:36:47 -0600

    feat(pi): enable cross platform builds, not just emulated builds

--- configurations/nixos/pi/system/nixpkgs.nix
+++ configurations/nixos/pi/system/nixpkgs.nix
@@ -1,6 +1,11 @@
 {
   # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
   nixpkgs = {
+    # https://blog.thalheim.io/2022/11/27/cross-compiling-and-deploying-nixos/
+    # This is the architecture we build from (pkgs.system from above)
+    buildPlatform = "x86_64-linux";
+    # pkgsCross.<yourtarget>.system
+    hostPlatform = "aarch64-linux";
     overlays = [
       (_: prev: {
         makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; });
