580fdb441cb9a72cf2a0b1b3e7d62e3fbd320b2e
Justin Hoang <j124.dev@proton.me>
2025-07-11 10:07:26 -0600

    feat(nginx): add base domain

--- configurations/nixos/lab/services/nginx.nix
+++ configurations/nixos/lab/services/nginx.nix
@@ -1,3 +1,7 @@
+{ config, ... }:
+let
+  inherit (config.networking) domain;
+in
 {
   services.nginx = {
     enable = true;
@@ -7,15 +11,29 @@
   };
 
   services.nginx.virtualHosts = {
-    # expose the ip address of the machine
-    "127.0.0.1" = {
-      locations."/ip" = {
-        extraConfig = ''
-          add_header Content-Type "application/json";
-        '';
-        return = ''
-          200 '{"host":"$server_name","ip":"$remote_addr","port":"$remote_port","server_ip":"$server_addr","server_port":"$server_port"}\n'
-        '';
+    "${domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      acmeRoot = null;
+      locations = {
+        "/" = {
+          return = ''
+            200 '<html><body>Hello World</body></html>'
+          '';
+          extraConfig = ''
+            add_header Content-Type text/html;
+          '';
+        };
+
+        # expose the ip address of the machine
+        "/ip" = {
+          return = ''
+            200 '{"host":"$server_name","ip":"$remote_addr","port":"$remote_port","server_ip":"$server_addr","server_port":"$server_port"}\n'
+          '';
+          extraConfig = ''
+            add_header Content-Type "application/json";
+          '';
+        };
       };
     };
   };
