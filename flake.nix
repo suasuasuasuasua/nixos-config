b2e23c19d546a8265d281bbd87ab00dbf3afbeac
Justin Hoang <j124.dev@proton.me>
2025-07-10 21:02:11 -0600

    build(flake): remove plasma-manager
    cool idea but too complicated for me imo

--- flake.nix
+++ flake.nix
@@ -47,11 +47,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim-config.url = "github:suasuasuasuasua/nixvim";
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
