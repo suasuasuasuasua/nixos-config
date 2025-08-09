cd0a8183abc498f09d31abc1ca54f8e75b7ff5f2
Justin Hoang <j124.dev@proton.me>
2025-08-08 22:55:39 -0600

    docs(starship): fix docs

--- modules/home/cli/starship.nix
+++ modules/home/cli/starship.nix
@@ -8,7 +8,9 @@ let
 in
 {
   options.custom.home.cli.starship = {
-    enable = lib.mkEnableOption "";
+    enable = lib.mkEnableOption ''
+      Minimal, blazing fast, and extremely customizable prompt for any shell
+    '';
   };
 
   config = lib.mkIf cfg.enable {
