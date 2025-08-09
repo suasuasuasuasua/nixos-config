9339883db9b2c39ce7022b3fd412923aecb4d6d2
Justin Hoang <j124.dev@proton.me>
2025-08-08 22:55:58 -0600

    feat(cli): added atuin cli db manager
    remembers commands

---
+++ modules/home/cli/atuin.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.custom.home.cli.atuin;
+in
+{
+  options.custom.home.cli.atuin = {
+    enable = lib.mkEnableOption ''
+      Replacement for a shell history which records additional commands context
+      with optional encrypted synchronization between machines
+    '';
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.atuin = {
+      enable = true;
+
+      enableZshIntegration = true;
+      settings = { };
+    };
+  };
+}
