f174e34495205e5a810a78377df10210ca1783c5
Justin Hoang <j124.dev@proton.me>
2025-08-15 11:46:24 -0600

    fix(obs): modularize obs to take package

--- modules/home/gui/obs.nix
+++ modules/home/gui/obs.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -9,10 +10,16 @@ in
 {
   options.custom.home.gui.obs = {
     enable = lib.mkEnableOption "Enable OBS";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.obs-studio;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     programs.obs-studio = {
+      inherit (cfg) package;
+
       enable = true;
       plugins = [ ];
     };
