8cbf5b99dfb98c9be988fa2d406628228755cae6
Justin Hoang <j124.dev@proton.me>
2025-10-18 06:55:06 -0600

    fix(nixvim): use unstable grug-far again
    not working on linux hosts i guess

--- configurations/home/penguin/nixvim.nix
+++ configurations/home/penguin/nixvim.nix
@@ -9,6 +9,7 @@ let
 
   nixvim = inputs.nixvim-config.packages.${system}.default.extend {
     config.plugins = {
+      grug-far.package = pkgs.unstable.vimPlugins.grug-far.nvim;
       neorg.settings.load."core.dirman".config = {
         workspaces = {
           "personal" = "${config.xdg.userDirs.documents}/vaults/personal";
