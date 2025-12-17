258997ecc3edca90c085fd5622de80b207b96154
Justin Hoang <justinhoang@sua.sh>
2025-12-17 06:44:30 -0700

    feat(lab): enable udisk for auto usb storage mounting

--- configurations/nixos/lab/system/storage.nix
+++ configurations/nixos/lab/system/storage.nix
@@ -17,4 +17,16 @@
       options = "--delete-older-than 30d";
     };
   };
+
+  # https://wiki.nixos.org/wiki/USB_storage_devices
+  #
+  # - udiskctl status
+  #   - find all available disks
+  # - udiskctl mount -b path/to/the/drive
+  #   - mount the drive
+  #   - remember to mount the partition, not the whole thing
+  #   - the location is under /run/media/$USER
+  # - udiskctl unmount -b path/to/the/drive
+  #   - unmount the drive
+  services.udisks2.enable = true;
 }
