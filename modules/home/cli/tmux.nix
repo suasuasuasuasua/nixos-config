0eb3950e5631706e100db71d79ad16db7626a53c
Justin Hoang <justinhoang@sua.sh>
2025-12-31 10:04:57 -0700

    fix(tmux): set tmux res dir

--- modules/home/cli/tmux.nix
+++ modules/home/cli/tmux.nix
@@ -26,6 +26,7 @@ in
             # tmux
             ''
               resurrect_dir=~/.tmux/resurrect/
+              set -g @resurrect-dir $resurrect_dir
               set -g @resurrect-hook-post-save-all "sed -i 's| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/nix/store/.*/bin/||g' $(readlink -f $resurrect_dir/last)"
 
               # restore the pane contents for sessions and for neovim
