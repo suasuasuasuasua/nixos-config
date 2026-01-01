40b741e9a9eb116da988871071506c0d4f51b559
Justin Hoang <justinhoang@sua.sh>
2026-01-01 11:01:21 -0700

    fix(mbp3): use fixed version of spotify
    https://github.com/NixOS/nixpkgs/pull/470535

--- configurations/darwin/mbp3/system/nixpkgs.nix
+++ configurations/darwin/mbp3/system/nixpkgs.nix
@@ -31,22 +31,5 @@
           "vscode-extension-ms-vscode-remote-remote-ssh-edit"
         ]
       );
-
-    overlays = [
-      # TODO: spotify build failure on darwin
-      # https://github.com/NixOS/nixpkgs/issues/465676
-      (_: prev: {
-        spotify = prev.spotify.overrideAttrs (oldAttrs: {
-          src =
-            if (prev.stdenv.isDarwin && prev.stdenv.isAarch64) then
-              prev.fetchurl {
-                url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
-                hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
-              }
-            else
-              oldAttrs.src;
-        });
-      })
-    ];
   };
 }
