6008d2abf05d507462685c2ef2b831fb839894e0
Justin Hoang <justinhoang@sua.dev>
2026-02-15 08:50:38 -0700

    build(darwin): use inet overlays
    https://github.com/NixOS/nixpkgs/issues/488689

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -29,7 +29,17 @@
     formatted;
 
   nixpkgs = {
-    overlays = builtins.attrValues outputs.overlays;
+    overlays = builtins.attrValues outputs.overlays ++ [
+      (_: super: {
+        inetutils = super.inetutils.overrideAttrs (attrs: rec {
+          version = "2.6";
+          src = super.fetchurl {
+            url = "mirror://gnu/${attrs.pname}/${attrs.pname}-${version}.tar.xz";
+            hash = "sha256-aL7b/q9z99hr4qfZm8+9QJPYKfUncIk5Ga4XTAsjV8o=";
+          };
+        });
+      })
+    ];
   };
 
   nix.enable = false;
