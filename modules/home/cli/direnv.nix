afe599f8498cdaa7dcbe8b4abe76b9032334445d
Justin Hoang <justinhoang@sua.dev>
2026-04-08 20:57:15 -0600

    fix(direnv): apply darwin patch

--- modules/home/cli/direnv.nix
+++ modules/home/cli/direnv.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.custom.home.cli.direnv;
 in
@@ -12,6 +17,12 @@ in
   config = lib.mkIf cfg.enable {
     programs.direnv = {
       enable = true;
+      # https://github.com/NixOS/nixpkgs/issues/507531
+      package = lib.mkIf pkgs.stdenv.isDarwin (
+        pkgs.direnv.overrideAttrs {
+          doCheck = false;
+        }
+      );
       enableZshIntegration = true;
       nix-direnv.enable = true; # faster implementation for nix
       # silent = true; # silent mode so it doesn't flood stdout
