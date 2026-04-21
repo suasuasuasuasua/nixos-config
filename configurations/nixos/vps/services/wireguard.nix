c41c8dd3edad3591b672543637c446c56b50b9d4
Justin Hoang <justinhoang@sua.dev>
2026-04-20 18:33:53 -0600

    chore: try using sops key

--- configurations/nixos/vps/services/wireguard.nix
+++ configurations/nixos/vps/services/wireguard.nix
@@ -1,15 +1,19 @@
 # WireGuard VPN server on VPS
-# This is the endpoint that lab and other servers connect to
+# This is the endpoint that lab connects to
 {
+  config,
+  inputs,
   pkgs,
   ...
 }:
 let
-  # Lab server's WireGuard IP and public key
-  # You'll need to generate keys and fill these in
   labPublicKey = "JVBP0NWpR70JT0bUoFsunFkGT9YZSY8O/UeMdUxAXlU=";
 in
 {
+  sops.secrets."wireguard/private_key" = {
+    sopsFile = "${inputs.self}/configurations/nixos/vps/secrets.yaml";
+  };
+
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
   };
@@ -20,10 +24,7 @@ in
       ips = [ "10.101.0.1/24" ];
       listenPort = 51820;
 
-      # Run on VPS after boot:
-      # $ wg genkey > /etc/wireguard/wg0.key
-      # $ chmod 600 /etc/wireguard/wg0.key
-      privateKeyFile = "/etc/wireguard/wg0.key";
+      privateKeyFile = config.sops.secrets."wireguard/private_key".path;
 
       peers = [
         {
