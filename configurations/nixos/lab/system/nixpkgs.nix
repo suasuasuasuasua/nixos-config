f0c2515c32068ed0d077d7b88a9a8b665c21221e
Justin Hoang <justinhoang@sua.dev>
2026-04-30 20:24:26 -0600

    fix(navidrome): remove overlay

--- configurations/nixos/lab/system/nixpkgs.nix
+++ configurations/nixos/lab/system/nixpkgs.nix
@@ -1,9 +1,4 @@
-{
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
+{ inputs, lib, ... }:
 {
   nixpkgs.config.allowUnfreePredicate =
     pkg:
@@ -13,13 +8,5 @@
       "minecraft-server"
     ];
 
-  nixpkgs.overlays = [
-    inputs.nix-minecraft.overlay
-    (self: _: {
-      navidrome = self.callPackage (pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/cimm/nixpkgs/71aa374ad541b41e6fccd543c67b6952d2ccafca/pkgs/by-name/na/navidrome/package.nix";
-        sha256 = "16mfj85w8d7vzc9pgcgjn7a71z7jywqpdn8igk9zp0hw9dvm9rmq";
-      }) { };
-    })
-  ];
+  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
 }
