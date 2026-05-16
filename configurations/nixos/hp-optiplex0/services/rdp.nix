ed18407b548b1cb4919df460a05a2352b3b127af
Justin Hoang <justinhoang@sua.dev>
2026-05-16 08:12:43 -0600

    fix: use proper gnome remote desktop name

--- configurations/nixos/hp-optiplex0/services/rdp.nix
+++ configurations/nixos/hp-optiplex0/services/rdp.nix
@@ -1,5 +1,5 @@
 {
-  services.gnome.remote-desktop.enable = true;
+  services.gnome.gnome-remote-desktop.enable = true;
 
   # Only allow RDP on the Tailscale interface (not exposed publicly)
   networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ 3389 ];
