4427fbb2020e75f420dfa9ef7d7552a8b77ffa88
Justin Hoang <justinhoang@sua.dev>
2026-05-25 09:03:27 -0600

    Update Nix flake inputs (#335)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Reviewed-on: https://gitea.sua.dev/sua/nixos-config/pulls/335

--- configurations/home/base.nix
+++ configurations/home/base.nix
@@ -1,4 +1,9 @@
-{ username, ... }:
+{ pkgs, username, ... }:
 {
   home.username = username;
+  home.homeDirectory =
+    let
+      root = if pkgs.stdenv.isDarwin then "/Users" else "/home";
+    in
+    "${root}/${username}";
 }
