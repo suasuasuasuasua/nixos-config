1da1a2baa78d698317132de5005c0a7394980bd7
Justin Hoang <justinhoang@sua.dev>
2026-05-25 18:34:26 -0600

    chore: try headless
    Signed-off-by: Justin Hoang <justinhoang@sua.dev>

--- configurations/nixos/hp-optiplex0/home/default.nix
+++ configurations/nixos/hp-optiplex0/home/default.nix
@@ -28,9 +28,30 @@
       };
     };
 
+    systemd.user.services.sway-headless = {
+      Unit = {
+        Description = "Sway Wayland compositor (headless)";
+        After = [ "default.target" ];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.sway}/bin/sway";
+        Environment = [
+          "WLR_BACKENDS=headless"
+          "WLR_RENDERER=pixman"
+          "WAYLAND_DISPLAY=wayland-1"
+          "XDG_SESSION_TYPE=wayland"
+          "XDG_CURRENT_DESKTOP=sway"
+        ];
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "default.target" ];
+    };
+
     services.wayvnc = {
       enable = true;
       autoStart = true;
+      systemdTarget = "sway-session.target";
       settings = {
         address = "0.0.0.0";
         port = 5900;
