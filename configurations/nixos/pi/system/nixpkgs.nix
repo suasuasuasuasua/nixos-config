4789df8a80a44e689e992342846ad1987e48a731
Justin Hoang <justinhoang@sua.dev>
2026-06-29 05:51:23 -0600

    Update Nix flake inputs (#340)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Reviewed-on: https://gitea.sua.dev/sua/nixos-config/pulls/340

--- configurations/nixos/pi/system/nixpkgs.nix
+++ configurations/nixos/pi/system/nixpkgs.nix
@@ -5,8 +5,10 @@
       (_: prev: {
         makeModulesClosure = x: prev.makeModulesClosure (x // { allowMissing = true; });
         # https://github.com/NixOS/nixpkgs/issues/500713
+        # pytestCheckHook runs in installCheckPhase, not checkPhase
         glances = prev.glances.overrideAttrs (_: {
           doCheck = false;
+          doInstallCheck = false;
         });
       })
     ];
